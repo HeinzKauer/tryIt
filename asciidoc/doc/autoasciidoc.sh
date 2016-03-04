15ce8b7007d0314e1ab8f1e475aec4b50b03a62c
Heinz Kauer <heinz.kauer@baloise.ch>
2016-03-04 13:48:33 +0100

    160403-1350

--- asciidoc/doc/autoasciidoc.sh
+++ asciidoc/doc/autoasciidoc.sh
@@ -3,7 +3,7 @@
 pwd=`pwd`
 relAsciiDocHome=`python -c "import os.path; print os.path.relpath('$ASCIIDOC_HOME', '$pwd')"`
 
-for f in ../doc/*.txt
+for f in *.txt
 do
 	echo `date +%T`: initial processing $f
 	python $ASCIIDOC_HOME/asciidoc.py -a asciidoc-dir=$relAsciiDocHome $@ $f
