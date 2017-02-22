9c96e70b2aa242ebbdcc888630b794089f77d67a
Heinz Kauer <heinz.kauer@baloise.ch>
2017-02-22 08:41:04 +0100

    save

--- shellScripts/kausoft.sh
+++ shellScripts/kausoft.sh
@@ -4,48 +4,38 @@
 # xxxxxxxxxxx
 
 
+# Info für cygwin
+# see C:\Users\B026670 -->  .bash_history .bashrc  .aliases
+# 
+
+kshelp(){
+
+    case "$1" in
+
+
+	* )	
+echo "help"
+	for dir in $(cat .mplist)
+		do
+			pushd "$dir" > /dev/null
+			if [ "$loglevel" == "0" ]; then echo -e "${COL_YELLOW}$dir... $COL_NONE"; fi
+			"$@"
+			if [ "$loglevel" == "0" ]; then echo -e "${COL_YELLOW}completed\n $COL_NONE"; fi
+			popd > /dev/null
+		done
+		;;
+	esac
+}
 ks(){
-	_reload
-
-
-	if [ "$1" == "-l" ]
-	then 
+	_reload   
 	
-		case "$2" in
-			debug )		
-			echo "debug"
-			;;
-		* )
-			echo "*"
-		;;
-		esac
-	
-		export loglevel=1
-		shift
-	else
-		export loglevel=0
-	fi
-
+	case "$1" in 	
+	cdws 	) _cdws $2 ;;
+	b 		) _build $2 $3 ;;
+	build 	) _build $2 $3 ;;
 	
 
-	
-	case "$1" in
-		c ) 
-			echo "ks_config 1"
-			_config
-			export param2=$2
-			
-						echo "config 3  $1 $2  "
-			echo $1
-		;;
-		
-	l ) 
-			echo "Set Loglevel"
-		;;	
-	kausoft ) 
-	      echo "kausoft"
-	      source $KAUSOFT/kausoft.sh
-        ;;
+
 	add ) 
 		shift
 		for dir in "$@"
@@ -106,7 +96,13 @@ ks(){
 		done
 		;;
 
+	help )
+	    # alle Parameter weitergeben 
+ks	    _help $@
+		;;
+		
 	* )	
+	_help
 		for dir in $(cat .mplist)
 		do
 			pushd "$dir" > /dev/null
@@ -119,22 +115,182 @@ ks(){
 	esac
 }
 
-# -------------------------------------------------------
-
-_config() {
-			echo "config 72  $1 "
-			echo "param2= $param2"
-}
-
+# ------------------------------------------------------- Aktualisieren von diesem Script.
 _reload(){
 	source $KAUSOFT/kausoft.sh
-	export message="reloadet "
-#	_log	
+	#export message="reloadet "
 }
 
 _log() {
 	if [ "$loglevel" == "0" ]; then echo -e "l-$loglevel ${COL_YELLOW}$message\n $COL_NONE"; fi
 }
+
+
+_cdws() { 
+    #echo "Set Workspace "$1		
+
+	case "$1" in	
+		master )
+			export NEWBLD_SPACE=$CDRIVE/Data/GitRepos/wsBLDMaster
+			export ORACLE_HOME=$CDRIVE/Dev/WLS12.1.1		
+			cd  /cygdrive/c/Data/GitRepos/wsBLDMaster > /dev/null		
+		;;
+
+		neon1 )
+			export NEWBLD_SPACE=$CDRIVE/Data/GitRepos/BLD_ws_neon_1
+			export ORACLE_HOME=$CDRIVE/Dev/WLS12.1.1		
+            cd  /cygdrive/c/Data/GitRepos/BLD_ws_neon_1 > /dev/null					
+		;;
+		
+	    * )				
+			echo "" 
+			echo "Help:" 
+			echo "ks cdws master = setzen Workspace und Git-Repo auf wsBLDMaster "
+			echo "ks cdws neon1  = setzen Workspace und Git-Repo auf BLD_ws_neon_1 "
+		;;
+	esac
+	
+	echo "" 
+	echo "Feedback:ks cdws "$1
+	echo "NEWBLD_SPACE = " $NEWBLD_SPACE
+	echo "ORACLE_HOME  = " $ORACLE_HOME
+	echo "-------------------------------------------------"
+}
+
+
+
+_build() {
+    export BUILD_TEST=-DskipTests
+	if [ "$2" == "-t" ]; then export BUILD_TEST=""; fi	
+	case "$1" in	
+	
+		pws )
+			cd $NEWBLD_SPACE/prevoWebService
+			mvn clean install $BUILD_TEST
+		;;
+
+    	pal )
+			cd $NEWBLD_SPACE/prevoAccessLayer
+			mvn clean install $BUILD_TEST
+		;;
+
+		wsa )
+			cd $NEWBLD_SPACE/webServiceAccess
+			mvn clean install $BUILD_TEST
+		;;
+
+		z )
+			cd $NEWBLD_SPACE/zie2klService
+			mvn clean install $BUILD_TEST
+		;;
+
+		g )
+			cd $NEWBLD_SPACE/gwtBase
+			mvn clean install $BUILD_TEST
+		;;
+
+		m )
+			cd $NEWBLD_SPACE/myKLBVG
+			mvn clean install $BUILD_TEST
+		;;
+
+		n )
+			cd $NEWBLD_SPACE/NewBLD
+			mvn clean install $BUILD_TEST
+		;;
+		
+		g+ )
+			cd $NEWBLD_SPACE/gwtBase
+			mvn clean install $BUILD_TEST
+			cd $NEWBLD_SPACE/myKLBVG
+			mvn clean install $BUILD_TEST
+			cd $NEWBLD_SPACE/NewBLD
+			mvn clean install $BUILD_TEST
+		;;
+
+		m+ )
+			cd $NEWBLD_SPACE/myKLBVG
+			mvn clean install $BUILD_TEST
+			cd $NEWBLD_SPACE/NewBLD
+			mvn clean install $BUILD_TEST
+		;;
+
+		n+ )
+			cd $NEWBLD_SPACE/NewBLD
+			mvn clean install $BUILD_TEST
+		;;
+		
+		
+		
+		
+	    * )				
+			echo "" 
+			echo "Syntax: "
+            echo "b|build [MODULE] [PARAMETER]" 
+			echo "" 
+			echo "MODULE:" 			
+			echo "pws = prevoWebService" 
+			echo "pal = prevoAccessLayer" 
+			echo "wsa = webServiceAccess" 
+			echo "z = zie2klService" 
+			echo "g = gwtBase" 
+			echo "m = myKLBVG" 
+			echo "n = NewBLD"
+			echo "g+ = gwtBase,myKLBVG,NewBLD" 
+			echo "m+ = myKLBVG,NewBLD" 
+			echo "n+ = NewBLD" 			
+			echo "" 
+			echo "PARAMETER:" 
+			echo "-t = Test ausführen "
+		;;
+	esac	
+}
+
+
+# ***********************************  help **************************************
+# Funktion ks help parameter
+# ***********************************  help **************************************
+_help() {
+    source $KAUSOFT/kausoft.sh    
+	
+	
+   	case "$2" in	
+	    # 
+		param )	
+			echo "ks- 170213-0819 --- help "$2 "-----------------------"
+			# Ausgeben der gesetzten Variablen 
+			echo "KAUSOFT        = "$KAUSOFT
+			echo "loglevel       = "$loglevel
+	
+			echo "MAVEN_HOME     = "$MAVEN_HOME
+			echo "JAVA_HOME      = "$JAVA_HOME
+			echo "ASCIIDOC_HOME  = "$ASCIIDOC_HOME
+			echo "CDRIVE         = "$CDRIVE
+			echo "KLTOOLS        = "$KLTOOLS
+			echo "NEWBLD_SPACE   = "$NEWBLD_SPACE
+			echo "ORACLE_HOME    = "$ORACLE_HOME
+		;;
+	
+	
+	
+	
+		* )	
+			echo "ks- 170213-0822 --- help "$2 "-----------------------"
+			echo "Folgende Parameter können an help übergeben werden"
+			echo "param        = Auflisten der gesetzten Variablen  "			
+			echo "cdws         = Setzen der Arbeitsumgebung "			
+			echo			
+			echo "in ycgwin werden folgende Files geladen --> .basch_profile .bashrc .profile  .aliases  --> C:\Users\B026670"
+			
+			
+			
+		;;
+
+	esac
+	
+	
+}
+
 
 
 
